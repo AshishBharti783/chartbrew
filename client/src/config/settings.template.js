ebb6efde9d8de948f50c4f9703faf25f28569e4c
AshishBharti783 <71437783+AshishBharti783@users.noreply.github.com>
2023-10-04 16:38:07 +0530

    Update settings.template.js

--- client/src/config/settings.template.js
+++ client/src/config/settings.template.js
@@ -1,10 +1,10 @@
 export const API_HOST = process.env.NODE_ENV === "production"
   ? process.env.REACT_APP_API_HOST
-  : (process.env.REACT_APP_API_HOST_DEV || "http://52.66.240.213:4019");
+  : (process.env.REACT_APP_API_HOST_DEV || "http://localhost:4019");
 
 export const SITE_HOST = process.env.NODE_ENV === "production"
   ? process.env.REACT_APP_CLIENT_HOST
-  : (process.env.REACT_APP_CLIENT_HOST_DEV || "http://52.66.240.213:4018");
+  : (process.env.REACT_APP_CLIENT_HOST_DEV || "http://localhost:4018");
 
 export const DOCUMENTATION_HOST = process.env.NODE_ENV === "production" ? "https://docs.chartbrew.com" : "http://localhost:8080";
 
